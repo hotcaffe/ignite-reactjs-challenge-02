c7ebcc26d7534d6438cccb5dc7bf882f81129a7e
Raphael Fusco <raphaelfusco.rar@gmail.com>
2022-05-13 22:20:31 -0300

    Finalizado

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,33 @@
-export function SideBar() {
-  // Complete aqui
+import {Button} from './Button'
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SideBarProps{
+  handleClickButton: (id: number) => void;
+  genres: GenreResponseProps[];
+  selectedGenreId: number;
+}
+
+export function SideBar({handleClickButton, genres, selectedGenreId}: SideBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  )
 }
